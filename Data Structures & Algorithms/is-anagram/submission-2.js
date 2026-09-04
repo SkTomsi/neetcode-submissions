0afa03b71623d64cf1f7633e8b1d7ae8a16fd6de
Tomcy Thomas <tomcy48@gmail.com>
2026-09-04 14:17:42 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        if (
+            s
+                .split("")
+                .sort((a, b) => a.localeCompare(b))
+                .join() !==
+            t
+                .split("")
+                .sort((a, b) => a.localeCompare(b))
+                .join()
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+}
